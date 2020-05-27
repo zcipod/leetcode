ae02639981661a172c6cca4abc4dec3288b7e5ee
zcipod <zcipod@163.com>
2020-05-27 14:33:45 +1000

    plus at the target list, to avoid too much operation on list and string. reverse the list to improve readability use int type to save the temp result, to avoid too much transformation between int and string
    Result:
    Runtime: 156 ms, faster than 35.13% of Python3 online submissions for Multiply Strings.
    Memory Usage: 13.8 MB, less than 7.14% of Python3 online submissions for Multiply Strings.

--- MultiplyStrings.py
+++ MultiplyStrings.py
@@ -30,65 +30,24 @@ from typing import List
 import decorator_time
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
-    def string_add_one(self, pre: List) -> List:
-        """let the suffix add one"""
-        if len(pre) == 0:
-            return ['1']
-        if int(pre[-1]) < 9:
-            pre[-1] = str(int(pre[-1]) + 1)
-            return pre
-        else:
-            return self.string_add_one(pre[:-1]) + ['0']
-
-    def add_string(self, a: List, b: List) -> List:
-        """add two list(num)
-        plus all the vertical results"""
-        if len(a) < len(b):
-            a, b = b, a
-        if len(b) == 0:
-            return a
-        res = ['0'] * (len(a) + 1)
-        add_one = 0
-        for i in range(1,len(b) + 1):
-            temp = int(b[-i]) + int(a[-i]) + add_one
-            add_one = 0
-            if temp >= 10:
-                temp -= 10
-                add_one = 1
-            res[-i] = str(temp)
-        for i in range(len(b) + 1, len(a) + 1):
-            temp = int(a[-i]) + add_one
-            add_one = 0
-            if temp >= 10:
-                temp -= 10
-                add_one = 1
-            res[-i] = str(temp)
-        res[0] = str(add_one)
-        return res if res[0] != '0' else res[1:]
-
     def multiply(self, num1: str, num2: str) -> str:
+        """plus in the target list, avoid too much operation of list and string"""
         if num1 == '0' or num2 == '0':
             return '0'
-        if len(num1) > len(num2):
-            num1, num2 = num2, num1
-        matrix = [[int(i) * int(j) for j in num2] for i in num1]
-        res = [0] * len(num1)
+        res = [0] * (len(num1) + len(num2))
+        num1 = list(num1)[::-1]
+        num2 = list(num2)[::-1]
         for i in range(len(num1)):
-            res[i] = list(str(matrix[i][0]))
-            for j in range(1, len(num2)):
-                if matrix[i][j] >= 10:
-                    temp = int(res[i][-1]) + matrix[i][j] // 10
-                    if temp >= 10:
-                        res[i] = self.string_add_one(res[i][:-1]) + list(str(temp)[-1]) + list(str(matrix[i][j])[-1])
-                    else:
-                        res[i][-1] = str(temp)
-                        res[i].append(str(matrix[i][j] % 10))
-                else:
-                    res[i].append(str(matrix[i][j]))
-        total = []
-        for i in range(len(num1)):
-            total = self.add_string(total, (res[i] + ['0'] * (len(num1) - 1 - i)))
-        return ''.join(total)
+            for j in range(len(num2)):
+                product = int(num1[i]) * int(num2[j]) + res[i + j]
+                res[i + j] = product % 10
+                res[i + j + 1] += product // 10
+        if res[-1] >= 10:
+            res[-1] %= 10
+            res.append(res[-1] // 10)
+        res = [str(i) for i in res[::-1]]
+        return "".join(res).lstrip('0')
+
 # leetcode submit region end(Prohibit modification and deletion)
 
 
